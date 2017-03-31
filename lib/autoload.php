dabeb14821059bdddb06d64e3031578008d0724d
Abbas Shakiba <shakiba_9@yahoo.com>
2017-03-31 11:32:09 +0430

    Add psr-0 autoload

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,7 +1,22 @@
 <?php
 require __DIR__ . '/config.php';
 require __DIR__ . '/RedBeanPHP4_3_4/rb.php';
+require __DIR__ . '/varDumpAsText.php';
 
+
+spl_autoload_register(function($class_name){
+    $base = __DIR__. '/../' .str_replace('\\', '/', $class_name).'Class.php';
+    $path = realpath($base);
+    if ($path) {
+        require_once $path;
+    }
+    else {
+        $e = new Exception();
+        print_r($e->getMessage());
+//        print_r($e->getTraceAsString());
+    }
+    return class_exists($class_name, false);
+}, true);
 
 R::setup( 'mysql:host='.$db_config['host'].';dbname='.$db_config['name'],
     $db_config['user'], $db_config['pass'] ); //for both mysql or mariaDB
